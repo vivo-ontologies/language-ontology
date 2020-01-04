70c1908f612af33dda1108cc8ad1909dab325b82
Michael Conlon <mconlon@ufl.edu>
2020-01-04 11:54:05 -0500

    CEFR Language capability definitions in de, es, fr, and it.  Minor improvements to i18n python.

--- src/ontology/i18n/README.md
+++ src/ontology/i18n/README.md
@@ -1,3 +1,17 @@
-In this folder we have files and tools related to adding i18n labels to the language ontology.
+In this folder we have files and tools related to adding i18n labels to the
+language ontology.
 
-* `vocabularyiso639-2.ttl` is the LOC linked data for iso639-2.  It cantains french, genrman, and english labels for each of the iso-639 languages.  Note:  we have no source for language labels for iso639-2.  If you have a source that can be reused, please open an issue and let us know.
+* `vocabularyiso639-2.ttl` is the LOC linked data for iso639-2.  It cantains
+french, genrman, and english labels for each of the iso-639 languages.  Note: 
+we have no source for language labels for iso639-2.  If you have a source that
+can be reused, please open an issue and let us know. 
+* `iso-639-i18n-generator.py` is a small python program that reads the LOC linked
+data, the LANG terms, and writes `iso-639-i18n.nt` a collecton of triples for
+asserting the french and german labels for the language terms.  
+* `iso-639-i18n.nt` assertions regarding the names of the languages in french and german.
+This file is merged withe other sources to create the language ontology. 
+* `iso-639-i18n-CEFR-definitions.nt` hand edited triples specifying the CEFR capability
+definitions in french, german, spanish, and italian.  Cut and Paste from the CEFR
+web site.  CEFR does not provide definitions for the "plus" capabilities, such as B2+. In
+each case, the defintion of the "non plus" capability is used withe the word "PLUS" added
+to the definition for the plus capability.
--- src/ontology/i18n/iso-639-i18n-generator.py
+++ src/ontology/i18n/iso-639-i18n-generator.py
@@ -3,8 +3,9 @@
 
 """
     iso-639-i18n-generator.py: read the LOC ttl to a graph.  Select labels to a dict, keyed on iso-639-2 code.
-    For each fr label, match the iso-639-2 to the lang ontology and output a triple with the iri of the language,
-    rdfs:label as the predicate, and the french label.  Repeat for the german labels.
+    Read the LOC translation data to a graph.  For each fr label, match the iso-639-2 to the lang ontology and output
+    a triple with the iri of the language, rdfs:label as the predicate, and the french label.  Repeat for the german
+    labels.
 """
 
 __author__ = "Michael Conlon"
@@ -15,55 +16,60 @@ __version__ = "0.1.0"
 from rdflib import Graph, URIRef
 from rdflib.namespace import SKOS, RDFS, Namespace
 
-obo = Namespace("http://purl.obolibrary.org/obo/")
+OBO = Namespace("http://purl.obolibrary.org/obo/")
+
 
 def main():
 
     uri = {}
     new_uri = {}
 
+    #  Read the LOC translation data a graph.  These have the french and german translations and the codes.
+
     g = Graph()
     g.parse("vocabularyiso639-2.ttl", format="ttl")
-    print("lines in ISO vocab", len(g))
+    print("Assertions in LOC vocabulary", len(g))
 
+    # Read the language terms to a graph.  These have the LANG IRI, and the codes.
 
     g1 = Graph()
     g1.parse('../templates/iso-639-terms.owl', format="xml")
     g1s = Graph()
     for s, p, o in g1:
-        if p == obo.LANG_0000003:  # Part 2T code
+        if p == OBO.LANG_0000003:  # Part 2T code
             g1s.add((s, RDFS.label, o))
             uri[str(o)] = s
-            print(s, p, o)
     for s, p, o in g1:
-        if p == obo.LANG_0000004:  # Part 2B Code
+        if p == OBO.LANG_0000004:  # Part 2B Code
             g1s.add((s, RDFS.label, o))
             uri[str(o)] = s
-            print(s, p, o)
     g1s.serialize('out.nt', format='nt')
 
-    print(len(uri))
+    print("Assertions in LANG terms", len(uri))
 
-    print(sorted(uri.keys()))
-
-
+    #   Construct a dictionary for translating LOC IRI to LANG IRI
 
     g2 = Graph()
 
     for s, p, o in g:
-        if p == SKOS.notation:
-            if str(o) in uri.keys():
+        if p == SKOS.notation:  # the three letter codes for the languages are skos:notations in the LOC data
+            if str(o) in uri.keys():  # look up the code in the dictionary constructed from LANG terms
                 new_uri[s] = uri[str(o)]
             else:
-                print('key in LOC', str(o), 'not found in lang terms')
+                print('key in LOC', str(o), 'not found in lang terms')  # not founds appear to be due deprecated
+                # language codes.  LANG has all languages, but not all deprecated codes for non-existent languages.
 
-    print(new_uri)
+    # Construct a graph to contain the new assertions.  Each assertion will be of the form
+    #
+    # <LANG_IRI> rdfs:label "label text"@language
+    #
+    # where language is either fr or de
 
     for s, p, o in g:
         if p == SKOS.prefLabel:
             if hasattr(o, 'language') and (o.language == 'fr' or o.language == 'de'):
                 if s in new_uri.keys():
-                    g2.add((new_uri[s], RDFS.label, o))
+                    g2.add((new_uri[s], RDFS.label, o))  # translate the LOC IRI to the LANG IRI
 
     print(len(g2), 'labels to add')
     g2.serialize("iso-639-i18n.nt", format="nt")
